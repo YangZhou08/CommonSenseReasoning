5acf550e583277935430cbcfdb74a55a448ca965
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-10 05:56:39 -0700

    return

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ def stop_sequences_criteria(
 
 for task in tasks: 
     dataloader, cotprompt = get_dataset(task) 
-    promptids = tokenizer(cotprompt, return_tensor = "pt", truncation = True, padding = False)["input_ids"] 
+    promptids = tokenizer(cotprompt, return_tensors = "pt", truncation = True, padding = False)["input_ids"] 
     for batch in dataloader: 
         input_ids = batch["input_ids"] 
         input_ids = torch.cat([promptids, input_ids], dim = 1) 
