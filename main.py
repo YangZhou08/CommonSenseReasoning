cd7f88931e4020c31e9414b3f1602de053f0c708
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-10 18:22:01 -0700

    key

--- main.py
+++ main.py
@@ -140,10 +140,10 @@ for task in tasks:
     promptids = torch.tensor(promptids, dtype = torch.long) 
     print("shape of promptids {}".format(promptids.shape)) 
     for batch in dataloader: 
-        print("answer found {}".format("answerKey" in batch.keys())) 
-        print(batch["answerKey"][0]) 
-        print(len(batch["answerKey"])) 
-        exit(0) 
+        # print("answer found {}".format("answerKey" in batch.keys())) 
+        # print(batch["answerKey"][0]) 
+        # print(len(batch["answerKey"])) 
+        # exit(0) 
         input_ids = batch["input_ids"] 
         print("length of input_ids: ", len(input_ids)) 
         print("length of input_ids[0]: ", len(input_ids[0])) 
