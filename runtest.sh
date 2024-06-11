9be35b3de429af07075b653372905c03c0c93145
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-11 16:20:16 -0700

    pythonsingle

--- runtest.sh
+++ runtest.sh
@@ -1,2 +1,2 @@
 # accelerate launch --main_process_port 29502 --num_processes 4 main.py --tasks csqa --model meta-llama/Meta-Llama-3-8B-Instruct --griffin=True --check=True --kernel_size 16 --spr 0.5 --thr 0.1 
-main.py --tasks csqa --model meta-llama/Meta-Llama-3-8B-Instruct --griffin=True --check=True --kernel_size 16 --spr 0.5 --thr 0.1 
+python main.py --tasks csqa --model meta-llama/Meta-Llama-3-8B-Instruct --griffin=True --check=True --kernel_size 16 --spr 0.5 --thr 0.1 
