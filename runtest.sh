e177e6f1328f9a675874747603e4af3054b1d635
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-16 09:32:26 -0400

    llama27b

--- runtest.sh
+++ runtest.sh
@@ -1,3 +1,3 @@
 # accelerate launch --main_process_port 29502 --num_processes 4 main.py --tasks csqa --model meta-llama/Meta-Llama-3-8B-Instruct --griffin=True --check=True --kernel_size 16 --spr 0.5 --thr 0.1 
-python main.py --tasks aqua --model meta-llama/Meta-Llama-3-8B-Instruct --shotfive | tee log8b.log 
-python main.py --tasks aqua --model meta-llama/Llama-2-7b-chat-hf --shotfive | tee log7b.log 
+python main.py --tasks aqua --model meta-llama/Meta-Llama-3-8B-Instruct --shotfive 
+python main.py --tasks aqua --model meta-llama/Llama-2-7b-chat-hf --shotfive 
