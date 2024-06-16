6bf0b18070fff57e9563f1fa6b39e48419b238da
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-16 09:30:51 -0400

    stop

--- main.py
+++ main.py
@@ -329,6 +329,8 @@ for task in tasks:
     ) 
     kv_cache = outputs.past_key_values 
     ''' 
+    # stop_criteria = stop_sequences_criteria(tokenizer, "Q:", input_ids.shape[1], input_ids.shape[0]) 
+    stop_criteria = stop_sequences_criteria(tokenizer, "Q: ", input_ids.shape[1], input_ids.shape[0]) 
     
     for i, batch in enumerate(tqdm(dataloader)): 
         # print("answer found {}".format("answerKey" in batch.keys())) 
@@ -342,14 +344,12 @@ for task in tasks:
             print(tokenizer.decode(input_ids[0])) 
         input_ids = torch.cat([promptids, input_ids], dim = 1) 
         input_ids = input_ids.to(args.device) 
-        stop_criteria = stop_sequences_criteria(tokenizer, "Q:", input_ids.shape[1], input_ids.shape[0]) 
-        # stop_criteria = stop_sequences_criteria(tokenizer, "Q: ", input_ids.shape[1], input_ids.shape[0]) 
         if is_distributed: 
             outputs = model.module.generate(
                 input_ids = input_ids, 
                 attention_mask = None, 
                 # max_length = input_ids.shape[1] + 20, 
-                max_length = input_ids.shape[1] + 200, 
+                max_length = input_ids.shape[1] + 500, 
                 use_cache = True, 
                 stopping_criteria = stop_criteria, 
                 pad_token_id = tokenizer.pad_token_id, 
@@ -360,7 +360,7 @@ for task in tasks:
             outputs = model.generate(
                 input_ids = input_ids, 
                 attention_mask = None, 
-                max_length = input_ids.shape[1] + 300, 
+                max_length = input_ids.shape[1] + 500, 
                 use_cache = True, 
                 stopping_criteria = stop_criteria, 
                 pad_token_id = tokenizer.pad_token_id, 
