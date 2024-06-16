6c7c83f7e7b07f8ed6133025766e4198310f2e82
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-16 04:43:01 -0400

    m" gi

--- main.py
+++ main.py
@@ -355,7 +355,7 @@ for task in tasks:
             outputs = model.generate(
                 input_ids = input_ids, 
                 attention_mask = None, 
-                max_length = input_ids.shape[1] + 200, 
+                max_length = input_ids.shape[1] + 300, 
                 use_cache = True, 
                 stopping_criteria = stop_criteria, 
                 pad_token_id = tokenizer.pad_token_id, 
