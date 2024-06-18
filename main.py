e91f6a25cafcff739e6648a15bd91b76e0d1b751
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-18 10:44:14 -0400

    cudlfkj

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def compensatingdataset(dataset):
             example["keep"] = "n" 
             return example 
         datasetdummy = datasetdummy.map(addingsignal) 
-        dataset = dataset.set_format([essentialkeys]) 
+        dataset = dataset.set_format(essentialkeys) 
         datasetdummy = datasetdummy.set_format([essentialkeys]) 
         dataset = concatenate_datasets([dataset, datasetdummy]) 
         return dataset 
