007d29acadb1745fd8a560868670ff4cfc1f71b8
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-07-01 15:26:43 -0400

    adding

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import lm_eval
 from datasets import load_dataset 
 # from transformers import AutoTokenizer, LlamaForCausalLM 
 from transformers import AutoTokenizer 
-from llama10 import get_llama_griffin, get_llama_griffin2, LlamaForCausalLM 
+# from llama10 import get_llama_griffin, get_llama_griffin2, LlamaForCausalLM 
+from llama12 import get_llama_griffin, get_llama_griffin2, LlamaForCausalLM 
+from llama12 import LlamaForCausalLM 
 import numpy as np 
 from datasets import concatenate_datasets 
 from datasets import Dataset 
