6c3e4a6ddf337d31e66b96722e37744927b28131
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-07-03 16:12:35 -0400

    using

--- main.py
+++ main.py
@@ -89,7 +89,8 @@ model.config.filteractiveenabled = args.filteractiveenabled
 if args.griffin: 
     model = get_llama_griffin2(model, schedule_k) 
 if args.cats: 
-    model = get_llama_griffin(model, schedule_k, patternstrict = args.patternstrict) 
+    # model = get_llama_griffin(model, schedule_k, patternstrict = args.patternstrict) 
+    model = get_llama_griffin(model, schedule_k) 
 
 model.eval() 
 if is_distributed: 
