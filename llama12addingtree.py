c727b7819069f7176d60f118ffbc086d199b20d3
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-07-10 13:51:01 -0400

    m

--- llama12addingtree.py
+++ llama12addingtree.py
@@ -1910,13 +1910,17 @@ class LlamaForCausalLM(LlamaPreTrainedModel):
                     from transformers.generation.stopping_criteria import MaxLengthCriteria 
                     for stoppingc in stopping_criteria: 
                         if isinstance(stoppingc, MaxLengthCriteria): 
+                            print("MaxLengthCriteria {}".format(stoppingc(input_ids, scores))) 
                             recheckoutcome = recheckoutcome or stoppingc(input_ids, scores) 
                         
                         if isinstance(stoppingc, MultiTokenEOSCriteria): 
                             stoppingc.done_tracker = [False] * input_ids.shape[0] 
+                            print("MultiTokenEOSCriteria {}".format(stoppingc(input_ids, scores))) 
                             recheckoutcome = recheckoutcome or stoppingc(input_ids, scores) 
                     if self.config.check: 
                         this_peer_finished = recheckoutcome 
+                        print("this_peer_finished {}".format(this_peer_finished)) 
+                        exit(0) 
 
             else: 
                 this_peer_finished = False 
