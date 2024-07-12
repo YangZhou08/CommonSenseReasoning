dc7648235be322164ec4b4b9224898c109ffd192
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-07-12 14:51:20 -0400

    using1800

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ parser.add_argument("--shotfive", action = "store_true")
 parser.add_argument("--shottwo", action = "store_true") 
 parser.add_argument("--filteractiveenabled", action = "store_true") 
 
-os.environ['NCCL_TIMEOUT'] = '7200'  # For 2 hours 
+os.environ['NCCL_TIMEOUT'] = '1800'  # For 2 hours 
 os.environ['TORCH_NCCL_BLOCKING_WAIT'] = '1'
 print("NCCL_TIMEOUT {}".format(os.environ['NCCL_TIMEOUT'])) 
 
