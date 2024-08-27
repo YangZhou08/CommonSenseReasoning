b230b4cbeddcf0aabd3dc1d76a9301c71041995c
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-08-27 14:06:33 -0400

    running

--- runtest.sh
+++ runtest.sh
@@ -25,11 +25,11 @@
 # accelerate launch --main_process_port 29501 --num_processes 8 main.py --tasks aqua --model meta-llama/Meta-Llama-3-8B-Instruct --shotfive --griffin --check --kernel_size 14 --spr 0.5 --thr 0.1 --widthtree 1 
 # accelerate launch --main_process_port 29501 --num_processes 8 main.py --tasks aqua --model meta-llama/Meta-Llama-3-8B-Instruct --shotfive --griffin --check --kernel_size 14 --spr 0.5 --thr 0.1 --widthtree 4 
 # accelerate launch --main_process_port 29501 --num_processes 8 main.py --tasks aqua --model meta-llama/Meta-Llama-3-8B-Instruct --shotfive --griffin --check --kernel_size 12 --spr 0.5 --thr 0.1 --widthtree 1 
-treesizes=(1 4 6 8) 
-for treesize in "${treesizes[@]}" 
-do 
-accelerate launch --main_process_port 29501 --num_processes 8 main.py --tasks csqa --model meta-llama/Meta-Llama-3-8B-Instruct --shotfive --griffin --check --kernel_size 16 --spr 0.5 --thr 0.05 --widthtree 1 
-done 
+treesizes=(4 6 8)
+for treesize in "${treesizes[@]}"
+do
+accelerate launch --main_process_port 29501 --num_processes 8 main.py --tasks csqa --model meta-llama/Meta-Llama-3-8B-Instruct --shotfive --griffin --check --kernel_size 16 --spr 0.5 --thr 0.05 --widthtree $treesize
+done
 # accelerate launch --main_process_port 29501 --num_processes 8 main.py --tasks aqua --model meta-llama/Meta-Llama-3-8B-Instruct --shotfive --griffin --check --kernel_size 12 --spr 0.5 --thr 0.1 --widthtree 8 
 # accelerate launch --main_process_port 29501 --num_processes 8 main.py --tasks aqua --model meta-llama/Meta-Llama-3-8B-Instruct --shotfive --griffin --check --kernel_size 12 --spr 0.5 --thr 0.05 --widthtree 1 
 # accelerate launch --main_process_port 29501 --num_processes 8 main.py --tasks aqua --model meta-llama/Meta-Llama-3-8B-Instruct --shotfive --griffin --check --kernel_size 12 --spr 0.5 --thr 0.01 --widthtree 1 
