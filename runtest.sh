283089fb168d41ceebcb8f3672465e95ea2aa4a9
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-08-29 08:11:43 -0400

    running

--- runtest.sh
+++ runtest.sh
@@ -38,7 +38,7 @@
 # accelerate launch --main_process_port 29501 --num_processes 4 main.py --tasks sports --model meta-llama/Llama-2-13b-chat-hf --shotfive --griffin --check --kernel_size 12 --spr 0.5 --thr 0.1  --widthtree 4 
 treesize=(1 4 6 8)
 for treesizee in ${treesize[@]}; do 
-accelerate launch --main_process_port 29501 --num_processes $treesize main.py --tasks date --model meta-llama/Meta-Llama-3-8B-Instruct --shotfive --cats --check --kernel_size 16 --spr 0.5 --thr 0.1  --widthtree 1 --patternstrict 
+accelerate launch --main_process_port 29501 --num_processes 6 main.py --tasks date --model meta-llama/Meta-Llama-3-8B-Instruct --shotfive --cats --check --kernel_size 16 --spr 0.5 --thr 0.1  --widthtree $treesize --patternstrict 
 done 
 # accelerate launch --main_process_port 29501 --num_processes 4 main.py --tasks sports --model meta-llama/Llama-2-13b-chat-hf --shotfive --griffin --check --kernel_size 12 --spr 0.5 --thr 0.05  --widthtree 4 
 # accelerate launch --main_process_port 29501 --num_processes 8 main.py --tasks aqua --model meta-llama/Llama-2-7b-hf --shotfive --griffin --check --kernel_size 16 --spr 0.5 --thr 0.01 --widthtree 6 
